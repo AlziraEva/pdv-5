19ea303c57e6894869d964025bf027c604addd60
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-10-11 11:20:38 -0300

    Pequenos ajustes nos controladores de usuários

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -3,7 +3,7 @@ const knex = require('../conexao');
 const jwt = require('jsonwebtoken');
 
 const cadastrarUsuario = async (req, res) => {
-    const { nome, email, senha } = req.body
+    const { nome, email, senha } = req.body;
 
     try {
         const emailExiste = await knex('usuarios').where({email}).first();
@@ -18,13 +18,13 @@ const cadastrarUsuario = async (req, res) => {
             nome,
             email,
             senha: senhaCriptografada
-        }).returning('*');
+        });
 
-        if(!novoUsuario[0]){
-            return res.status(400).json('O usuário não foi cadastrado.');
+        if(!novoUsuario){
+            return res.status(400).json({mensagem: 'O usuário não foi cadastrado.'});
         }
 
-        return res.status(201).json(novoUsuario[0]);
+        return res.status(201).json({mensagem: 'O usuário foi cadastrado.'});
 
     } catch (error) {
         return res.status(500).json({ mensagem: 'Erro interno no servidor' });
@@ -38,26 +38,28 @@ const efetuarLoginDoUsuario = async(req, res) => {
         const usuario = await knex('usuarios').where({email}).first();
 
         if(!usuario){
-            return res.status(401).json('Email e/ou senha inválidos.');
+            return res.status(401).json({mensagem: 'Email e/ou senha inválidos.'});
         }
 
         const senhaCorreta = await bcrypt.compare(senha, usuario.senha);
 
         if (!senhaCorreta){
-            return res.status(401).json('Email e/ou senha inválidos.');
+            return res.status(401).json({mensagem: 'Email e/ou senha inválidos.'});
         }
 
         const token = jwt.sign({id: usuario.id }, process.env.JWT_SECRET, {expiresIn: '1h'} );
 
         const { senha: _, ...dadosUsuarioLogado } = usuario;
 
-        return res.status(200).json({
+        const usuarioLogado = {
             usuario: dadosUsuarioLogado,
             token
-        });
+        }
+
+        return res.status(200).json(usuarioLogado); 
 
     } catch (error) {
-        return res.status(500).json({mensagem: "Erro interno do servidor."})
+        return res.status(500).json({mensagem: "Erro interno do servidor."});
     }
 }
 
