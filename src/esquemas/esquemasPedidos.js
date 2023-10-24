e28f4bbafabb34ddeeb0acefd394b32fc550d4b7
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-10-24 19:56:28 -0300

    Adicionado o esquema de pedidos

--- src/esquemas/esquemasPedidos.js
+++ src/esquemas/esquemasPedidos.js
@@ -12,18 +12,39 @@ const esquemaPedidos = joi.object({
         'string.max': 'O campo observação precisa ter no máximo 255 caracteres.',
     }),
 
-    pedido_produtos: joi.array().items(joi.object({
-        produto_id: joi.number().integer().positive().required().messages({
-            'any.required': 'O campo produto_id é obrigatório.',
-            'number.base': 'O campo produto_id precisa ser um número inteiro positivo.',
-            'number.positive': 'O campo produto_id precisa ser um número inteiro positivo.',
+    pedido_produtos: joi.array()
+        .min(1)
+        .required()
+        .items(joi
+            .object({
+                produto_id: joi.number()
+                    .integer()
+                    .positive()
+                    .required()
+                    .messages({
+                        'any.required': 'O campo produto_id é obrigatório.',
+                        'number.base': 'O campo produto_id precisa ser um número inteiro positivo.',
+                        'number.positive': 'O campo produto_id precisa ser um número inteiro positivo.',
+                    }),
+
+                quantidade_produto: joi.number()
+                    .integer()
+                    .positive()
+                    .required()
+                    .messages({
+                        'any.required': 'O campo quantidade_produto é obrigatório.',
+                        'number.base': 'O campo quantidade_produto precisa ser um número inteiro positivo.',
+                        'number.positive': 'O campo quantidade_produto precisa ser um número inteiro positivo.',
+                    }),
+            })
+            .messages({
+                'object.base': 'O campo pedido_produtos precisa ter um formato válido.',
+            }))
+        .messages({
+            'any.required': 'O campo pedido_produtos é obrigatório.',
+            'array.base': 'O campo pedido_produtos precisa ter um formato válido.',
+            'array.min': 'O campo pedido_produtos deve conter no mínimo um produto.'
         }),
-        quantidade_produto: joi.number().integer().positive().required().messages({
-            'any.required': 'O campo quantidade_produto é obrigatório.',
-            'number.base': 'O campo quantidade_produto precisa ser um número inteiro positivo.',
-            'number.positive': 'O campo quantidade_produto precisa ser um número inteiro positivo.',
-        }),
-    })),
 
 });
 
