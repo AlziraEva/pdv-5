fb06920aea2ee5bff16a342c9759f6c52073e944
Karlasilvaeng <karlasilvaeng@gmail.com>
2023-10-25 10:26:19 -0300

    Criação do arquivo intermediariosPedidos.js e transferência do intermediário validarPedido para esse arquivo

--- src/intermediarios/intermediariosProdutos.js
+++ src/intermediarios/intermediariosProdutos.js
@@ -24,30 +24,6 @@ const verificarProdutoPorId = async (req, res, next) => {
     }
 };
 
-const validarPedido = async (req, res, next) => {
-    const { pedido_produtos } = req.body;
-    try {
-        for (const produto of pedido_produtos) {
-            const { produto_id, quantidade_produto } = produto;
-
-            const produtoExiste = await knex('produtos').where('id', produto_id).first();
-
-            if (!produtoExiste) {
-                return res.status(400).json({ mensagem: 'Não há produto cadastrado com o ID especificado.' })
-            }
-
-            if (produtoExiste.quantidade_estoque < quantidade_produto) {
-                return res.status(400).json({ mensagem: 'Quantidade em estoque insuficiente.' })
-            }
-        }
-
-        next();
-    } catch (erro) {
-        return res.status(500).json({ mensagem: 'Erro interno no servidor.' })
-    }
-};
-
 module.exports = {
-    verificarProdutoPorId,
-    validarPedido
+    verificarProdutoPorId
 }
